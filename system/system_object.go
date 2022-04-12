b4428f3b4b1add9d5b7e7af8d1c3b7f3bdbe88a8
bskim <20171579@edu.hanbat.ac.kr>
2022-04-12 16:47:18 +0900

    4.12

--- system/system_object.go
+++ system/system_object.go
@@ -1,1 +1,41 @@
-aaa
+package system_object
+
+import (
+	"fmt"
+	"time"
+)
+
+type SysObject struct {
+	__GLOBAL_OBJECT_ID int
+	__created_time     string
+	__object_id        int
+	__object_id_other  int
+}
+
+func (sy *SysObject) __init_() {
+	sy.__GLOBAL_OBJECT_ID = 0
+	sy.__created_time = time.Now().String()
+	sy.__object_id = sy.__GLOBAL_OBJECT_ID
+	sy.__GLOBAL_OBJECT_ID = sy.__GLOBAL_OBJECT_ID + 1
+	sy.__object_id_other = 0
+}
+
+func (sy *SysObject) __str__() string {
+	return fmt.Sprintf("ID:%10d %s", sy.__object_id, sy.__created_time)
+}
+
+func set_req_time(sy *SysObject) {
+	return
+}
+
+func get_req_time(sy *SysObject) {
+	return
+}
+
+func (sy *SysObject) __lt__() bool {
+	return sy.__object_id < sy.__object_id_other
+}
+
+func (sy *SysObject) get_obj_id() int {
+	return sy.__object_id
+}
