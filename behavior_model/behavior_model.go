3cff3bbfe62b01583d7b7a1d234e68f3a74f8763
bskim <20171579@edu.hanbat.ac.kr>
2022-04-13 16:48:22 +0900

    4.13

--- behavior_model/behavior_model.go
+++ behavior_model/behavior_model.go
@@ -1,9 +1,35 @@
 package behaviormodel
 
-type behaviormodel struct {
-	_states
-	external_transition_map_tuple
-	external_transition_map_state
-	internal_transition_map_tuple
-	internal_transition_map_state
+import (
+	"evsim_golang/definition"
+	"math"
+)
+
+type Behaviormodel struct {
+	_states                       map[string]float64
+	external_transition_map_tuple []string
+	external_transition_map_state []string
+	internal_transition_map_tuple []string
+	internal_transition_map_state []string
+	coreModel                     *definition.CoreModel
+}
+
+func (b *Behaviormodel) Insert_state(name string, deadline float64) { //deadline 디폴트값 = 0
+	if deadline == 0 {
+		deadline = math.Inf(1)
+	}
+	b._states[name] = deadline
+}
+
+func (b *Behaviormodel) Update_state(name string, deadline float64) { //deadline 디폴트값 = 0
+	if deadline == 0 {
+		deadline = math.Inf(1)
+	}
+	b._states[name] = deadline
+}
+
+func NewBehaviorModel(name string) *Behaviormodel {
+	b := Behaviormodel{}
+	b.coreModel = definition.NewCoreModel(name, 0)
+	return &b
 }
