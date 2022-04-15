885b1d7ff325a20f04abfc2a4314c4ef963ffe6c
bskim <20171579@edu.hanbat.ac.kr>
2022-04-15 22:11:07 +0900

    4.15

--- system/system_message.go
+++ system/system_message.go
@@ -1,4 +1,4 @@
-package system
+package system_object
 
 type SysMessage struct {
 	_src      string
@@ -7,30 +7,38 @@ type SysMessage struct {
 	_msg_list []string
 }
 
-func (b *SysMessage) insert(msg string) {
+func NewSysMessage(src_name string, dst_name string) *SysMessage {
+	sy := SysMessage{}
+	sy._src = src_name
+	sy._dst = dst_name
+	sy._msg_time = -1
+	return &sy
+}
+
+func (b *SysMessage) Insert(msg string) {
 	b._msg_list = append(b._msg_list, msg)
 }
 
-func (b *SysMessage) extend(_list []string) {
+func (b *SysMessage) Extend(_list []string) {
 	b._msg_list = append(b._msg_list, _list...)
 }
 
-func (b *SysMessage) retrieve() []string {
+func (b *SysMessage) Retrieve() []string {
 	return b._msg_list
 }
 
-func (b *SysMessage) get_src() string {
+func (b *SysMessage) Get_src() string {
 	return b._src
 }
 
-func (b *SysMessage) get_dst() string {
+func (b *SysMessage) Get_dst() string {
 	return b._dst
 }
 
-func (b *SysMessage) set_msg_time(t float64) {
+func (b *SysMessage) Set_msg_time(t float64) {
 	b._msg_time = t
 }
 
-func (b *SysMessage) get_msg_time() float64 {
+func (b *SysMessage) Get_msg_time() float64 {
 	return b._msg_time
 }
