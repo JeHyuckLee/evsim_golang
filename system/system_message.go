06b2ac03ed6f25bf4f438f30e4dd20e5f9e8898d
bskim <20171579@edu.hanbat.ac.kr>
2022-04-15 22:06:09 +0900

    4.15

---
+++ system/system_message.go
@@ -0,0 +1,36 @@
+package system
+
+type SysMessage struct {
+	_src      string
+	_dst      string
+	_msg_time float64
+	_msg_list []string
+}
+
+func (b *SysMessage) insert(msg string) {
+	b._msg_list = append(b._msg_list, msg)
+}
+
+func (b *SysMessage) extend(_list []string) {
+	b._msg_list = append(b._msg_list, _list...)
+}
+
+func (b *SysMessage) retrieve() []string {
+	return b._msg_list
+}
+
+func (b *SysMessage) get_src() string {
+	return b._src
+}
+
+func (b *SysMessage) get_dst() string {
+	return b._dst
+}
+
+func (b *SysMessage) set_msg_time(t float64) {
+	b._msg_time = t
+}
+
+func (b *SysMessage) get_msg_time() float64 {
+	return b._msg_time
+}
