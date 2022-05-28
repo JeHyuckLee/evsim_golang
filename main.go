019bc1e448734cc582312c8a7fdd1274fd0f3820
bskim <20171579@edu.hanbat.ac.kr>
2022-05-29 03:28:32 +0900

    5.29

--- main.go
+++ main.go
@@ -5,13 +5,26 @@ import (
 	"evsim_golang/executor"
 	"evsim_golang/system"
 	"fmt"
-
-	"time"
 )
 
 type Generator struct {
 	executor *executor.BehaviorModelExecutor
 	msg_list []interface{}
+}
+
+func NewGenerator(instance_time, destruct_time float64, name, engine_name string) *Generator {
+	gen := Generator{}
+	gen.executor = executor.NewExecutor(instance_time, destruct_time, name, engine_name)
+	gen.executor.AbstractModel = &gen
+	gen.executor.Init_state("IDLE")
+	gen.executor.Behaviormodel.Insert_state("IDLE", definition.Infinite)
+	gen.executor.Behaviormodel.Insert_state("MOVE", 1)
+	gen.executor.Behaviormodel.CoreModel.Insert_input_port("start")
+	gen.executor.Behaviormodel.CoreModel.Insert_output_port("process")
+	for i := 0; i < 10; i++ {
+		gen.msg_list = append(gen.msg_list, i)
+	}
+	return &gen
 }
 
 func (g *Generator) Ext_trans(port string, msg *system.SysMessage) {
@@ -32,84 +45,29 @@ func (g *Generator) Int_trans() {
 }
 
 func (g *Generator) Output() *system.SysMessage {
-	//fmt.Println("output")
-	msg := system.NewSysMessage(g.executor.Behaviormodel.CoreModel.Get_name(), "process")
-	msg.Insert(g.msg_list[0])
+
+	fmt.Println(g.msg_list...)
 	g.msg_list = remove(g.msg_list, 0)
-	return msg
-}
-
-func NewGenerator() *Generator {
-	gen := Generator{}
-	gen.executor = executor.NewExecutor(0, definition.Infinite, "Gen", "sname")
-	gen.executor.AbstractModel = &gen
-	gen.executor.Init_state("IDLE")
-	gen.executor.Behaviormodel.Insert_state("IDLE", definition.Infinite)
-	gen.executor.Behaviormodel.Insert_state("MOVE", 1)
-	gen.executor.Behaviormodel.CoreModel.Insert_input_port("start")
-	gen.executor.Behaviormodel.CoreModel.Insert_output_port("process")
-	for i := 0; i < 10; i++ {
-		gen.msg_list = append(gen.msg_list, i)
-	}
-	return &gen
-}
-
-type Processor struct {
-	executor *executor.BehaviorModelExecutor
-	msg_list []interface{}
-}
-
-func (p *Processor) Ext_trans(port string, msg *system.SysMessage) {
-	//fmt.Println("ext_trans")
-	if port == "process" {
-		p.executor.Cancel_rescheduling()
-		data := msg.Retrieve()
-		p.msg_list = append(p.msg_list, data...)
-		p.executor.Cur_state = "PROCESS"
-	}
-}
-
-func (p *Processor) Int_trans() {
-	//fmt.Println("int_trans")
-	if p.executor.Cur_state == "PROCESS" {
-		p.executor.Cur_state = "IDLE"
-	} else {
-		p.executor.Cur_state = "IDLE"
-	}
-}
-
-func (p Processor) Output() *system.SysMessage {
-	//fmt.Println("output")
-	fmt.Println("elapsed Time :", time.Since(executor.Start_time))
 	return nil
 }
 
-func NewProcessor() *Processor {
-	pro := &Processor{}
-	pro.executor = executor.NewExecutor(0, definition.Infinite, "Proc", "sname")
-	pro.executor.AbstractModel = pro
-	pro.executor.Init_state("IDLE")
-	pro.executor.Behaviormodel.Insert_state("IDLE", definition.Infinite)
-	pro.executor.Behaviormodel.Insert_state("PROCESS", 1)
-	pro.executor.Behaviormodel.CoreModel.Insert_input_port("PROCESS")
-
-	return pro
-}
-
 func main() {
-	fmt.Println("start", time.Now())
-	executor.Start_time = time.Now()
+	se := executor.NewSysSimulator()            // 엔진 생성
+	se.Register_engine("sname", "REAL_TIME", 1) // 엔진 등록
+	sim := se.Get_engine("sname")               // 엔진 가져오기
 
-	se := executor.NewSysSimulator()
-	se.Register_engine("sname", "REAL_TIME", 1)
-	sim := se.Get_engine("sname")
-	sim.Behaviormodel.CoreModel.Insert_input_port("start")
-	gen := NewGenerator()
-	sim.Register_entity(gen.executor)
-	sim.Coupling_relation(nil, "start", gen.executor, "start")
+	sim.Behaviormodel.CoreModel.Insert_input_port("start") // 입력 포트 생성
 
-	sim.Insert_external_event("start", nil, 0)
-	sim.Simulate(definition.Infinite)
+	gen := NewGenerator(0, definition.Infinite, "Gen", "sname") // 정의한 오브젝트 가져오기
+	// 오브젝트의 생성시간, 파괴시간, 이름, 등록할 엔진이름
+
+	sim.Register_entity(gen.executor) // 오브젝트 등록
+
+	sim.Coupling_relation(nil, "start", gen.executor, "start") // 엔진 포트와 오브젝트 포트 연결
+
+	sim.Insert_external_event("start", nil, 0) // 발생시킬 이벤트 정의
+
+	sim.Simulate(definition.Infinite) // 엔진 실행
 }
 
 func remove(slice []interface{}, s int) []interface{} {
